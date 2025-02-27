cc6f3a2a60245f22bf2898240668650b0a7dae48
Willy Watcho <107815299+nkwilly@users.noreply.github.com>
2025-02-27 03:00:47 +0100

    Update register method

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -44,15 +44,13 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  register: async (username, password, email) => {
+  register: async (login, password, email) => {
     try {
       const response = await fetch(`${baseURLAPI}/auth/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password, email }),
+        body: JSON.stringify({ login, password, email }),
       });
-
-      console.log(response); 
 
       if (!response.ok) {
         throw new Error('Registration failed');
