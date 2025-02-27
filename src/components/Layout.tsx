231363daf16797c459fb15412c4f38330e450832
Willy Watcho <107815299+nkwilly@users.noreply.github.com>
2025-02-27 03:45:11 +0100

    Update logout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,19 @@
-import { Outlet, Link, useLocation } from 'react-router-dom';
+import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom';
 import { LogOut, GiftIcon, CoinsIcon } from 'lucide-react';
 import { useAuthStore } from '../store/authStore';
 
 export function Layout() {
   const location = useLocation();
   const { user, logout } = useAuthStore();
+
+  const onSubmit = async () => {
+    try {
+      await logout(); 
+      navigate("/login"); 
+    } catch(e) {
+      console.error(e); 
+    }
+  }
 
   return (
       <div className="min-h-screen bg-gray-50">
@@ -53,7 +62,7 @@ export function Layout() {
               <div className="flex items-center">
                 <span className="text-gray-700 mr-4">{user?.email}</span>
                 <button
-                    onClick={logout}
+                    onClick={onSubmit}
                     className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <LogOut className="h-4 w-4 mr-2" />
